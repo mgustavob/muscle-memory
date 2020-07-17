905e66c7c84530e5886f3a9332bd1b1a03f72f48
Martin Briceno <martin.briceno@live.com>
2020-07-17 17:36:40 -0400

    10 functions

--- app.js
+++ app.js
@@ -11,12 +11,56 @@
 // };
 
 let sentence = "Today is Friday!;"
+
 let age = 42;
+
 const motorcycles = ['Kawasaki', 'Yamaha', 'Suzuki', 'Honda'];
+
 let doesThisWork = true;
+
 const lakers = {
     headquarters: "Los Angeles",
     numberOfPlayes: 11,
     players: ['LBJ', 'Anthony Davis', 'Danny Green'],
     areChampions: true
+}
+function passArray (array1) {
+    console.log('This function passes and array');
+
+}
+
+function passString(name1) {
+    console.log('I take the string and put it here: ' +name1);
+};
+
+function passObject(object1) {
+
+}
+
+function boolean1(ifTrue) {
+    if (if True == true) {
+        console.log(false);
+    } else {
+        console.log('false');
+    }
+}
+
+function sum1(numb1, numb2) {
+    return numb1 + numb2;
+}
+
+function multi1(numb1, numb2) {
+    return numb1 * numb2;
+}
+
+function substrac1(numb1, numb2) {
+    return numb1 - numb2;
+}
+
+function yourName(name1) {
+    console.log('Your name is : ' +name1);
+}
+
+function favColor(input1) {
+    console.log('your favorite color is: ' +input1)
 }
