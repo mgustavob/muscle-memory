ea359bb9ecf7a3b6c193eea6aebf50d51b66f18e
Martin Briceno <martin.briceno@live.com>
2020-07-17 17:07:57 -0400

    [variables] add 5 variables

---
+++ app.js
@@ -0,0 +1,22 @@
+// var name1 = "Martin";
+// var age = 27;
+// var array1 = [3, "Tony", 43, "Star"];
+// var todayMonday = false;
+// var object1 = {
+//     brand: "toyota",
+//     model: "camry",
+//     [3, 6, "today"],
+//     isGrey: true,
+//     year: 2020,
+// };
+
+let sentence = "Today is Friday!;"
+let age = 42;
+const motorcycles = ['Kawasaki', 'Yamaha', 'Suzuki', 'Honda'];
+let doesThisWork = true;
+const lakers = {
+    headquarters: "Los Angeles",
+    numberOfPlayes: 11,
+    players: ['LBJ', 'Anthony Davis', 'Danny Green'],
+    areChampions: true
+}
